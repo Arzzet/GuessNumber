906b5d0b0cf9b2e2d033e501194e05ec9c874362
Iwo <iwoskater3@hotmail.com>
2021-12-13 08:05:09 +0100

    fix fotos

--- app/src/main/java/com/example/guessnumber/MainActivity.java
+++ app/src/main/java/com/example/guessnumber/MainActivity.java
@@ -2,14 +2,19 @@ package com.example.guessnumber;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.DialogFragment;
 
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.icu.text.SimpleDateFormat;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
+import android.provider.MediaStore;
 import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
@@ -17,19 +22,26 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.guessnumber";
-    static int intentCount;
-    EditText numberInput;
-    int guessedNumber;
-    Random r = new Random();
-    int secretNumber;
-    boolean isOn = false;
-    int mili=0, seg=0, min=0;
-    String mi = "", s = "", m = "";
-    Handler h = new Handler();
+    private static int intentCount;
+    private EditText numberInput;
+    private int guessedNumber;
+    private Random r = new Random();
+    private int secretNumber;
+    private boolean isOn = false;
+    private int mili=0, seg=0, min=0;
+    private String mi = "", s = "", m = "";
+    private Handler h = new Handler();
+    private File imagen = null;
+    private Intent intent;
+    private String path;
+    private Editable valueName;
 
 
     @Override
@@ -119,7 +131,7 @@ public class MainActivity extends AppCompatActivity {
 
             if (guessedNumber == secretNumber) {
                 Toast.makeText(context, "Has acertado", duration).show();
-                Intent intent = new Intent(this, Results.class);
+                intent = new Intent(this, Results.class);
 //                String message = "Nº intentos: " + intentCount;
                 AlertDialog.Builder alert = new AlertDialog.Builder(this);
 
@@ -132,10 +144,15 @@ public class MainActivity extends AppCompatActivity {
 
                 alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int whichButton) {
-                        Editable value = input.getText();
-                        String message = value + "," + intentCount + ","+ min + ":" + seg + ":" + mili;
-                        intent.putExtra(EXTRA_MESSAGE, message);
-                        startActivity(intent);
+                        valueName = input.getText();
+                        try {
+                            abrirCamara();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+//                        String message = valueName + "," + intentCount + ","+ min + ":" + seg + ":" + mili;
+//                        intent.putExtra(EXTRA_MESSAGE, message);
+//                        startActivity(intent);
                     }
                 });
 
@@ -169,6 +186,61 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("Intentos: " + intentCount);
 
     }
+
+    private File createImageFile() throws IOException {
+
+        String timeStamp = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        }
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        imagen = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+
+        path = imagen.getAbsolutePath();
+        return imagen;
+    }
+    private void abrirCamara() throws IOException {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        File photoFile = createImageFile();
+
+        Uri photoURI = FileProvider.getUriForFile(this,
+                "com.iwo.android.fileprovider",
+                photoFile);
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+        startActivityForResult(takePictureIntent, 1);
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Uri fileUri = null;
+        System.out.println("Imagen cargada:"+ imagen.toString());
+        fileUri = Uri.fromFile(imagen);
+
+
+        String message = valueName + "," + intentCount + ","+ min + ":" + seg + ":" + mili + "," + imagen.getAbsolutePath();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+//        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+//            Bundle extras = data.getExtras();
+//            Bitmap imageBitmap = (Bitmap) extras.get("data");
+//            imageView.setImageBitmap(imageBitmap);
+//        }
+    }
+
+
+
+}
 //    public class PuntosDialog extends DialogFragment {
 //        @Override
 //        public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -190,7 +262,7 @@ public class MainActivity extends AppCompatActivity {
 //        }
 //    }
 
-}
+
 
 //        System.out.println(guessedNumber);
 
@@ -229,9 +301,9 @@ public class MainActivity extends AppCompatActivity {
 //        button.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View view) {
-
-
-
+//
+//
+//
 //                Context context = getApplicationContext();
 //                CharSequence text = "Olivares dale Gas!";
 //                int duration = Toast.LENGTH_LONG;
